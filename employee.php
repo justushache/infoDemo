4b772e60877ca1fd5691248088e2649f5636f7d2
TheoFrk <33786175+TheoFrk@users.noreply.github.com>
2020-06-20 10:42:36 +0200

    Add files via upload

--- employee.php
+++ employee.php
@@ -1,35 +1,44 @@
 <?php
 
-class Employee(){
+class Employee{
     private $firstName;
     private $lastName;
-    private $birthYear;
+    private $age;
     private $salery;
     private $position;
 
-    function __construct($firstName,$lastName,$birthYear,$salery,$position){
-        $this->firsName = $firstName;
+    function __construct($firstName,$lastName,$age,$salery,$position){
+        $this->firstName = $firstName;
         $this->lastName = $lastName;
-        $this->birthYear = $birthYear;
+        $this->age = $age;
         $this->salery = $salery;
         $this->position = $position;
     }
 
-        public function getFirstName()
+    public function getFirstName()
     {
-
-    } 
+        return $this->firstName;
+    }
 
     public function getLastName()
     {
-
+        return $this->lastName;
     }
 
     public function getAge()
     {
+        return $this->age;
+    }
 
-}
+    public function getSalery()
+    {
+        return $this->salary;
+    }
 
+    public function getPosition()
+    {
+        return $this->position;
+    }
 
 
 

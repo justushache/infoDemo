ddc5154b7af3969034ccab3ea58e82532ab14769
Justus <justushache@yahoo.com>
2020-06-20 10:15:17 +0200

    finish constructors

---
+++ firm.php
@@ -0,0 +1,16 @@
+<?php
+
+class firm(){
+    private $name;
+    private $worth;
+    private $year_of_creation;
+
+    __construct($name,$worth,$year_of_creation){
+        $this->name = $name;
+        $this->worth = $worth;
+        $this->year_of_creation = $year_of_creation;
+    }
+    
+}
+
+?>
--- mitarbeiter.php
+++ mitarbeiter.php
@@ -1,9 +1,22 @@
 <?php
 
-class Mitarbeiter(){
+class Employee(){
+    private $firstName;
+    private $lastName;
+    private $birthYear;
+    private $salery;
+    private $position;
+
+    function __construct($firstName,$lastName,$birthYear,$salery,$position){
+        $this->firsName = $firstName;
+        $this->lastName = $lastName;
+        $this->birthYear = $birthYear;
+        $this->salery = $salery;
+        $this->position = $position;
+    }
 
 
-    
+
 
 }
 
